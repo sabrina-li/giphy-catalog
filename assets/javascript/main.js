8b04b10d95a6364c41c8faa1234c52ddc6fa6e7a
Sabrina L <sabrinali0312@gmail.com>
2019-03-09 11:41:28 -0500

    add sidenav inital and vscode test config

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,2 +1,11 @@
-var array = [];
+var array = ["ski","swim","running","cliff jumping"];
 
+$(document).ready(function(){
+    function initSideBar(){
+        array.forEach(function(val){
+            console.log(val);
+            $(".sidenav").append("<a>"+val+"</a>");
+        })
+    }
+    initSideBar();
+});
